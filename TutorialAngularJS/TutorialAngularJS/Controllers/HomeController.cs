f0adae0bcbce4f8a7a908148cfa8349c24e6ec4a
Irving Herrera <irving.herreramolina@gmail.com>
2018-06-04 13:09:06 -0500

    ng app, ng controller y scope

--- TutorialAngularJS/TutorialAngularJS/Controllers/HomeController.cs
+++ TutorialAngularJS/TutorialAngularJS/Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace TutorialAngularJS.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult ControlScope()
+        {
+            return View();
+        }
     }
 }
